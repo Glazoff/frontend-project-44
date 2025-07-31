13274f95f85042820e1b68ea3870e7fb8811b1a5
Glazoff <glazov_evgen@mail.ru>
2025-07-31 19:49:08 +0300

    step_7/add_game_gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
+import { runGame } from '../src/index.js'
+import generateQuestions, { description } from '../src/games/gcd.js'
 
-console.log('brain-gcd')
+const rounds = generateQuestions()
+runGame(description, rounds)
